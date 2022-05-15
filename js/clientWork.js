e9af17c65577bc29d9aa610f15ac452aee3b182e
jorgetmz <jorgetmz@outlook.com>
2022-05-14 21:20:46 -0500

    Integración de nenlaces a otras vistas.

---
+++ js/clientWork.js
@@ -0,0 +1,7 @@
+const buttonCreate = document.getElementsByClassName('title-option__button')[0];
+
+function redirectCreateWork(){
+    window.location.href = '../newWork.html';
+}
+
+buttonCreate.addEventListener('click', redirectCreateWork);
